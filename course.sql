c7e187e39b2eb1a02e3774bf64c034efbd9f1486
hilbert25 <hht@pku.edu.cn>
2018-10-09 11:26:39 +0800

    add course

--- course.sql
+++ course.sql
@@ -1,1 +1,31 @@
-INSERT INTO `course`(`id`, `courseIndex`, `courseHomeWork`) VALUES (1, 3, '333');
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : test
+ Source Server Type    : MySQL
+ Source Server Version : 80012
+ Source Host           : localhost:3306
+ Source Schema         : homework
+
+ Target Server Type    : MySQL
+ Target Server Version : 80012
+ File Encoding         : 65001
+
+ Date: 09/10/2018 11:26:23
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `courseIndex` int(255) DEFAULT NULL,
+  `courseHomeWork` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
