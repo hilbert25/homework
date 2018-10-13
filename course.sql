757e1752d1c5aa94e6b5b3384ecba921ea088937
hilbert25 <hht@pku.edu.cn>
2018-10-13 18:54:23 +0800

    update sql

--- course.sql
+++ course.sql
@@ -1,1 +1,31 @@
-INSERT INTO `course`(`id`, `courseIndex`, `courseHomeWork`) VALUES (1, 2, '›]ÓÐ×÷ÒµÀ²À²À²À²À²');
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : test
+ Source Server Type    : MySQL
+ Source Server Version : 50723
+ Source Host           : localhost:3306
+ Source Schema         : homework
+
+ Target Server Type    : MySQL
+ Target Server Version : 50723
+ File Encoding         : 65001
+
+ Date: 13/10/2018 18:54:06
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for course
+-- ----------------------------
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `courseIndex` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `courseHomeWork` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- homework.sql
+++ homework.sql
@@ -3,15 +3,15 @@
 
  Source Server         : test
  Source Server Type    : MySQL
- Source Server Version : 80012
+ Source Server Version : 50723
  Source Host           : localhost:3306
  Source Schema         : homework
 
  Target Server Type    : MySQL
- Target Server Version : 80012
+ Target Server Version : 50723
  File Encoding         : 65001
 
- Date: 07/10/2018 22:46:25
+ Date: 13/10/2018 18:53:59
 */
 
 SET NAMES utf8mb4;
@@ -23,13 +23,14 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `homework`;
 CREATE TABLE `homework`  (
   `id` int(8) NOT NULL AUTO_INCREMENT,
-  `stuId` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `stuName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `homeWorkTitle` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `homeWorkPath` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  `homeWorkSubmitTime` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0),
-  `homeWorkIndex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stuId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `stuName` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `homeWorkTitle` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `homeWorkPath` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `homeWorkSubmitTime` datetime(0) DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP(0),
+  `homeWorkIndex` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `fingerPrint` text CHARACTER SET utf8 COLLATE utf8_general_ci,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
